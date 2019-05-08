e464746ea13a78a2aed75fb846ebeed3ff5d09c6
Hildi Radya <hildiradya@gmail.com>
2019-05-08 11:25:10 +0700

    Change iterate model

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,6 +2,6 @@ import UIKit
 
 var projects = ["2019 - iOS App", "2018 - MacOS App", "2017 - Web App"]
 
-for project in projects {
-    print(project)
+for i in 0...projects.count - 1 {
+    print(projects[i])
 }
